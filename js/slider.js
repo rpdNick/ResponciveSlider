af918ad2f04837b1a38d265423ef3f53deca6b2b
rpdNick <rpd_nick@ukr.net>
2021-01-21 17:10:06 +0200

    ready slider

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
+
+function init(){
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth + 12;
+    sliderLine.style.width = width * images.length + 'px';
+    console.log(width);
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    })
+   
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider-next').addEventListener('click', ()=> {
+    count ++;
+    if (count >= images.length){
+        count = 0;
+    }
+    rollSlider();
+});
+document.querySelector('.slider-prev').addEventListener('click', ()=> {
+    count --;
+    if (count < 0){
+        count = images.length -1;
+    }
+    rollSlider();
+})
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    console.log(sliderLine.style.transform = 'translate(-' + count * width + 'px)');
+}
